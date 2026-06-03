385663841e34f1bee39cbefd5f501987b8f76223
abhinavpingle06 <pingleabhinav@gmail.com>
2026-06-03 22:31:02 +0530

    feat1: Email Auth, feat2: Google Auth, feat3: Guest

---
+++ config/firebase.ts
@@ -0,0 +1,7 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!);
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
